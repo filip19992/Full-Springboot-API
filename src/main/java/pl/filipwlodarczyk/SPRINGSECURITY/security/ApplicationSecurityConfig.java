38179f1fde67ab99fb6472a17f6f0b5536e49d4e
filip19992 <filipwlodarczyk99@gmail.com>
2021-08-03 12:41:51 +0200

    Adding templates, editing login page

--- src/main/java/pl/filipwlodarczyk/SPRINGSECURITY/security/ApplicationSecurityConfig.java
+++ src/main/java/pl/filipwlodarczyk/SPRINGSECURITY/security/ApplicationSecurityConfig.java
@@ -3,7 +3,7 @@ package pl.filipwlodarczyk.SPRINGSECURITY.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -12,12 +12,13 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
-import static pl.filipwlodarczyk.SPRINGSECURITY.security.ApplicationPermissions.*;
 import static pl.filipwlodarczyk.SPRINGSECURITY.security.ApplicationRoles.*;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final PasswordEncoder passwordEncoder;
@@ -29,18 +30,21 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .csrf().disable()//TODO: Check in details
+                .csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/", "index", "/css/*", "/js/*").permitAll()
                 .antMatchers("/api/**").hasRole(STUDENT.name())
-                .antMatchers(HttpMethod.DELETE,"/management/api/**").hasAuthority(COURSE_WRITE.getPermission())
-                .antMatchers(HttpMethod.POST,"/management/api/**").hasAuthority(COURSE_WRITE.getPermission())
-                .antMatchers(HttpMethod.PUT,"/management/api/**").hasAuthority(COURSE_WRITE.getPermission())
-                .antMatchers(HttpMethod.GET, "/management/api/**").hasAnyRole(ADMIN.name(), ADMINTRAINEE.name())
+//                .antMatchers(HttpMethod.DELETE,"/management/api/**").hasAuthority(COURSE_WRITE.getPermission())
+//                .antMatchers(HttpMethod.POST,"/management/api/**").hasAuthority(COURSE_WRITE.getPermission())
+//                .antMatchers(HttpMethod.PUT,"/management/api/**").hasAuthority(COURSE_WRITE.getPermission())
+//                .antMatchers(HttpMethod.GET, "/management/api/**").hasAnyRole(ADMIN.name(), ADMINTRAINEE.name())
                 .anyRequest()
                 .authenticated()
                 .and()
-                .httpBasic();
+                .formLogin()
+                .loginPage("/login").permitAll().defaultSuccessUrl("/courses", true)
+                .and()
+                .rememberMe(); //default to 2 weeks
     }
 
     @Override
--- src/main/java/pl/filipwlodarczyk/SPRINGSECURITY/student/StudentManagementController.java
+++ src/main/java/pl/filipwlodarczyk/SPRINGSECURITY/student/StudentManagementController.java
@@ -1,6 +1,7 @@
 package pl.filipwlodarczyk.SPRINGSECURITY.student;
 
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
@@ -20,28 +21,26 @@ public class StudentManagementController {
     );
 
     @GetMapping
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_ADMINTRAINEE')")
     public List<Student> getAllStudents() {
         return STUDENTS;
     }
 
     @PostMapping
+    @PreAuthorize("hasAuthority('student:write')")
     public void registerNewStudent(@RequestBody Student student) {
         System.out.println(student);
     }
 
 
     @DeleteMapping(path = "{studentId}")
+    @PreAuthorize("hasAuthority('student:write')")
     public void deleteStudent(@PathVariable("studentId") Long studentId) {
-        for (Student student : STUDENTS) {
-            if (student.getStudentId().equals(studentId)) {
-                STUDENTS.remove(student);
-            } else {
-                throw new IllegalStateException("There is no student with id " + studentId);
-            }
-        }
+        STUDENTS.removeIf(student -> student.getStudentId().equals(studentId));
     }
 
     @PutMapping(path = "{studentId}")
+    @PreAuthorize("hasAuthority('student:write')")
     public void updateStudent(@PathVariable("studentId") Long studentId,@RequestBody Student student) {
         System.out.println(String.format("%s %s", studentId, student));
     }
